94020e061c7c28da435fd30eebbcdfb6385b889e
BoldCodeer <micsrelles@gmail.com>
2022-10-20 16:10:53 +0800

    first commit

---
+++ dist/types/index.d.ts
@@ -0,0 +1,4 @@
+import EventCalendar from "./EventCalendar";
+export { RestDataProvider } from "@xbs/scheduler-data-provider";
+export { EventCalendar };
+export { en, ru, de, defaultEditorShape, defaultCalendars, uid, } from "@xbs/lib-scheduler";
